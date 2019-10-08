0125d092e3b5acc726e479814674869323848ac6
KC <carpenter.katie.e@gmail.com>
2019-10-08 18:21:32 -0400

    uploaded file

--- index.js
+++ index.js
@@ -1,18 +1,56 @@
+function getMean(maximum, minimum) {
+  return Math.round((maximum + minimum) / 2)
+}
+
 const readline = require('readline');
-const rl = readline.createInterface(process.stdin, process.stdout);
+const readlineInterface = readline.createInterface(process.stdin, process.stdout);
+let guess = 50
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
-    rl.question(questionText, resolve);
-  });
+      readlineInterface.question(questionText, resolve);
+    });
 }
-
-start();
 
 async function start() {
   console.log("Let's play a game where you (human) make up a number and I (computer) try to guess it.")
-  let secretNumber = await ask("What is your secret number?\nI won't peek, I promise...\n");
+  let secretNumber = await ask("What is your secret number?");
   console.log('You entered: ' + secretNumber);
-  // Now try and complete the program.
+
+  let yesNo = await ask("Is your number " + guess)
+
+  while (yesNo !== 'y') {
+
+      if (yesNo.toLowerCase() === "n") {
+
+          let highLow = await ask("Is it higher or lower? ")
+
+          if (highLow.toLowerCase() === 'h') {
+
+              min = guess
+              max = 100
+              guess = getMean(max, min)
+              yesNo = await ask(guess)
+
+          } else if (highLow.toLowerCase() === 'l') {
+
+              max = guess
+              min = 1
+              guess = getMean(max, min)
+              yesNo = await ask(guess)
+
+          }
+
+      } else {
+
+          console.log("please enter y or n.")
+
+      }
+
+  }
+
+  console.log("Yay! I won")
   process.exit();
 }
+
+start();
